a48cacb124aba2e31ecc8c94e0b04d9085b139aa
Franco Morales Castro <franco.morales@outlook.com>
2024-07-08 22:09:46 -0400

    ✨ feat: add GET, PUT and DELETE methods

--- application/tasks/routes.py
+++ application/tasks/routes.py
@@ -7,7 +7,17 @@ tasks_blueprint = Blueprint("tasks", __name__, url_prefix="/api")
 @tasks_blueprint.route("/tasks", methods=["GET"])
 def get_tasks():
     tasks = Task.query.all()
-    return jsonify([{ "id": task.id, 'title': task.title } for task in tasks])
+    return jsonify([{ "id": task.id, 'title': task.title, 'completed': task.completed } for task in tasks])
+
+@tasks_blueprint.route("/tasks/<int:task_id>", methods=["GET"])
+def get_task_by_id(task_id):
+    task = Task.query.get_or_404(task_id)
+    return jsonify({
+        "id": task.id,
+        "title": task.title,
+        "description": task.description,
+        "completed": task.completed
+    })
 
 @tasks_blueprint.route("/tasks", methods=["POST"])
 def create_task():
@@ -20,3 +30,24 @@ def create_task():
     return jsonify({
         "message": "Task created successfully"
     }), 201
+
+@tasks_blueprint.route("/tasks/<int:task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    data = request.get_json()
+    task.title = data.get("title", task.title)
+    task.description = data.get("description", task.description)
+    task.completed = data.get("completed", task.completed)
+    db.session.commit()
+    return jsonify({
+        "message": "Task updated successfully"
+    }), 204
+
+@tasks_blueprint.route("/tasks/<int:task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({
+        "message": "Task deleted successfully"
+    })
