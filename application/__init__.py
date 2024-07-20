3e89a0bf8fe90af8ff397756d2292317887fcd5a
Franco Morales Castro <franco.morales@outlook.com>
2024-07-19 20:10:00 -0400

    feat: add contact on app

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from application.extensions import db, migrate
 from application.tasks.routes import tasks_blueprint
+from application.contacts.routes import contacts_blueprint
 from flask_cors import CORS
 
 def create_app():
@@ -10,4 +11,5 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     app.register_blueprint(tasks_blueprint)
+    app.register_blueprint(contacts_blueprint)
     return app
